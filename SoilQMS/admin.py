40eedf6f1c7356b569ff674c54fb4e217ff7db14
Miraz J Naina <42742560+miraz00@users.noreply.github.com>
2024-03-20 17:41:00 +0530

    added crop related tables

--- SoilQMS/admin.py
+++ SoilQMS/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Crop, UserCrop, UserCropHistory
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Crop)
+admin.site.register(UserCrop)
+admin.site.register(UserCropHistory)
--- SoilQMS/models.py
+++ SoilQMS/models.py
@@ -1,5 +1,35 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Crop(models.Model):
+    name = models.CharField(max_length=64)
+    ph_value = models.FloatField()
+    temperature = models.FloatField()
+    nutrients = models.FloatField()
+    moisture = models.FloatField()
+
+
+class UserCrop(models.Model):
+    ideal_crop = models.OneToOneField(Crop, on_delete=models.CASCADE, related_name='user_measured_values')
+    ph_value = models.FloatField()
+    temperature = models.FloatField()
+    nutrients = models.FloatField()
+    moisture = models.FloatField()
+
+
+class UserCropHistory(models.Model):
+    crop = models.ForeignKey(UserCrop, on_delete=models.CASCADE, related_name='reports')
+    ph_value = models.FloatField()
+    temperature = models.FloatField()
+    nutrients = models.FloatField()
+    moisture = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "UserCropHistory"
+
